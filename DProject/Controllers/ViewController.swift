0252b20e75c7e3b4cb83ad3850f3c322635be260
mehul modhvadiya <modhvadiya.mehul@gmail.com>
2020-03-18 10:58:19 +0530

    first commit

---
+++ DProject/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DProject
+//
+//  Created by mehul modhvadiya on 12/03/20.
+//  Copyright © 2020 mehul modhvadiya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DProject/Model/TodoModel.swift
@@ -0,0 +1,9 @@
+//
+//  TodoModel.swift
+//  DProject
+//
+//  Created by mehul modhvadiya on 12/03/20.
+//  Copyright © 2020 mehul modhvadiya. All rights reserved.
+//
+
+import Foundation
